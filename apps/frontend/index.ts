f29132c39cadebe40947098c877789687a7650fe
philia <48457894+philia11@users.noreply.github.com>
2025-11-07 19:36:38 -0300

    fixes

--- apps/frontend/index.ts
+++ apps/frontend/index.ts
@@ -1,30 +1,24 @@
 import 'dotenv/config'
 
 import { html } from '@elysiajs/html'
+import { staticPlugin } from '@elysiajs/static'
 import { Elysia } from 'elysia'
 import path from 'path'
-import staticify from 'staticify'
 import { fileURLToPath } from 'url'
 
-import newsletter from './controllers/newsletter'
-import notFound from './controllers/notFound'
 import page from './controllers/page'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const app = new Elysia({ aot: false })
-app.use(html())
-
-const staticifyInstance = staticify(path.join(__dirname, 'build'))
-
-app
-  .decorate('getVersionedPath', staticifyInstance.getVersionedPath)
+  .use(html())
+  .use(staticPlugin({ assets: path.join(__dirname, 'build'), prefix: '/' }))
   .get('/', page)
-  .get('/:slug', page)
-  .get('/product/:slug', page)
-  .post('/signup', newsletter)
-  .get('*', notFound)
+  // .get('/:slug', page)
+  // .get('/product/:slug', page)
+  // .post('/signup', newsletter)
+  // .get('*', notFound)
 
 const PORT = process.env.PORT || 3000
 
